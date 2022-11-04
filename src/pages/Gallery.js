1929ebbbca4c66ad8f3e00188f3581e95fb90d61
tgashwinkumar <tgashwinkumar@gmail.com>
2022-11-04 18:28:25 +0530

    Setting up Gallery Accordion

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,12 +1,34 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import {
+  MdOutlineKeyboardArrowDown,
+  MdOutlineKeyboardArrowRight,
+} from "react-icons/md";
 import Layout from "./Layout";
 
 const Gallery = () => {
+  return (
+    <Layout>
+      <div className="p-8 w-full">
+        <h1 className="text-black tracking-wider text-4xl font-bold uppercase mb-4">
+          Gallery
+        </h1>
+        <GalleryAccordion title="Republic Day 2022" />
+        <GalleryAccordion title="Republic Day 2022" />
+        <div className="[column-width:33vw] md:[column-width:25vw] lg:[column-width:20vw] [column-gap:1rem] w-full mt-8 pr-8">
+          {/* <div className="grid  [grid-template-columns:repeat(auto-fill,250px)] [grid-auto-rows:10px] [justify-content:center]"> */}
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export const GalleryAccordion = ({ title = "" }) => {
   const [photos, setPhotos] = useState([]);
+
   useEffect(() => {
     axios
-      .get("https://picsum.photos/v2/list?limit=20&page=1", {})
+      .get("https://picsum.photos/v2/list?limit=7", {})
       .then((res) => {
         setPhotos(
           res.data.map((photo) => ({
@@ -19,27 +41,33 @@ const Gallery = () => {
       .catch((err) => console.log(err));
   }, []);
 
+  const [isHidden, setIsHidden] = useState(false);
+
   return (
-    <Layout>
-      <div className="p-8 w-full">
-        <h1 className="text-black tracking-wider text-4xl font-bold uppercase">
-          Gallery
-        </h1>
-        <div className="[column-width:33vw] md:[column-width:25vw] lg:[column-width:20vw] [column-gap:1rem] w-full mt-8 pr-8">
-          {/* <div className="grid  [grid-template-columns:repeat(auto-fill,250px)] [grid-auto-rows:10px] [justify-content:center]"> */}
+    <div className="w-full lg:p-4">
+      <div className="flex items-center space-x-4 mb-4">
+        <button onClick={(e) => setIsHidden(!isHidden)}>
+          {isHidden ? (
+            <MdOutlineKeyboardArrowRight size={36} />
+          ) : (
+            <MdOutlineKeyboardArrowDown size={36} />
+          )}
+        </button>
+        <h1 className="text-3xl font-semibold">{title}</h1>
+      </div>
+      {!isHidden && (
+        <div className="flex flex-wrap">
           {photos.map((photo) => (
-            <div className="inline-block w-full m-2">
-              <img
-              loading="lazy"
-                src={photo.image_url}
-                alt={photo.title}
-                className="w-full block"
-              />
-            </div>
+            <img
+              // loading="lazy"
+              src={photo.image_url}
+              alt={photo.title}
+              className="h-36 block"
+            />
           ))}
         </div>
-      </div>
-    </Layout>
+      )}
+    </div>
   );
 };
 
