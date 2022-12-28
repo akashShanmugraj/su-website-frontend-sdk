61a7694aad64a3e1604293dbcf7a5bcebf1b6774
tgashwinkumar <tgashwinkumar@gmail.com>
2022-12-28 11:23:11 +0530

    layout changes

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
   return (
     <div className="w-screen overflow-x-hidden">
       <Navbar />
-      <div className="pt-16 min-h-[calc(100vh-27rem)]">{children}</div>
+      <div className="pt-16 min-h-[calc(100vh-27rem)] px-8 lg:px-[calc(100vw/12)]">{children}</div>
       <ContactUs />
     </div>
   );
