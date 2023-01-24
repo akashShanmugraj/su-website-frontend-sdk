0e9852947aa01e54dc8564042839a8ba5ce9bd0e
sachitha1010 <sachitha1910@gmail.com>
2023-01-24 16:06:06 +0530

    SU feed update

--- src/API/config.js
+++ src/API/config.js
@@ -1,1 +1,3 @@
 export const API_URL = "http://localhost:3002/api"
+
+export const FEED_URL = `${API_URL}/club/proposal/all_published`
--- src/pages/SUFeed.js
+++ src/pages/SUFeed.js
@@ -1,15 +1,34 @@
+import React, { useContext, useEffect, useState } from "react";
 import { FiAlertCircle } from "react-icons/fi";
 import Layout from "./Layout.js";
 import { BiShareAlt } from "react-icons/bi";
+import axios from "axios";
+import { setDashPattern } from "pdf-lib";
+import { FEED_URL } from "../API/config";
 
 const SUFeed = () => {
+
+  const[event,setEvent]=useState();
+  const url = FEED_URL;
+  const sendrequest = async ()=>{
+    const res = await axios.get(`${FEED_URL}`).catch((err)=>console.log(err));
+    const data =await res.data;
+    return data;
+  }
+
+  useEffect(()=>{
+    sendrequest().then((data)=>setEvent(data));
+  },[]);
+
+  console.log(event);
+
   return (
     <Layout>
       <h1 className="text-4xl uppercase text-center mt-16 w-full">
         SU <span className="font-bold">Feed</span>
       </h1>
       <div className="flex flex-col lg:flex-row mt-8 w-full lg:space-x-8 px-8">
-        <div className="w-full lg:w-1/4 font-sans overflow-x-hidden">
+        <div className="w-full lg:w-1/4 font-sans overflow-x-hidden" >
           <h2 className="font-sans text-2xl">Alerts</h2>
           <p className="text-gray-400">No featured posts yet.</p>
         </div>
@@ -22,24 +41,22 @@ const SUFeed = () => {
               frequently for updates on various information.
             </p>
           </section>
+        {event && event.map((event,index)=>(
           <section className="w-full border-gray-400 border-2 rounded-lg">
             <div className="p-6 border-b-gray-400 border-b-2">
               <header className="flex items-center space-x-4">
                 <div className="rounded-full w-8 h-8 aspect-square bg-gray-600"></div>
                 <div className="">
                   <div className="flex items-center space-x-1">
-                    <p className=" font-semibold">Students Union</p>
+                    <p className=" font-semibold">{event.orgName}</p>
                     <p className="">:</p>
                     <p className=" text-gray-500">1d</p>
                   </div>
-                  <div className="text-sm text-gray-500">Be the Change</div>
+                  <div className="text-sm text-gray-500">{event.eventName}</div>
                 </div>
               </header>
               <p className="mt-4">
-                Our UG (Regular) 1997 batch alumni & UG (Sandwich) 1998 batch
-                alumni donated Rs 40 lakhs to PSG Tech Alumni Foundation as part
-                of the Silver Jubilee Celebrations of our college, PSG College
-                Of Technology .
+                {event.description}
               </p>
             </div>
             <div className="w-full">
@@ -52,6 +69,7 @@ const SUFeed = () => {
               <BiShareAlt size={36} className="text-gray-600 hover:text-gray-700" />
             </div>
           </section>
+))}
         </div>
         <div className="w-full lg:w-1/4 font-sans overflow-x-hidden">
           <h2 className="font-sans text-2xl">Spotlights</h2>
