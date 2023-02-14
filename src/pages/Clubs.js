baeba18982ab37164a25f0aab0f898a7357bca10
Akash Shanmugaraj <65720968+akashShanmugraj@users.noreply.github.com>
2023-02-14 10:42:10 +0530

    Added better Grid Layout with Image & Description

--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -5,6 +5,22 @@ import { Link } from "react-router-dom";
 import Layout from "./Layout.js";
 import { fetchAllClubsLogo } from "../API/calls.js";
 
+function ClubListing() {
+    return (
+
+            <div className={'max-w-sm bg-white border border-gray-200 rounded-3xl shadow dark:bg-gray-800 dark:border-gray-700'}>
+                <a>
+                    <img className={"rounded-t-lg"} src={"https://images.unsplash.com/photo-1577985051167-0d49eec21977?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2089&q=80"}/>
+                </a>
+                <div className={"p-5"}>
+                    <a href = "#">
+                        <h5 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">Book Readers Club</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Smells like paper, just outta press</p>
+                </div>
+            </div>
+    )
+}
 const Clubs = () => {
   const [clubs, setClubs] = useState([]);
 
@@ -19,32 +35,20 @@ const Clubs = () => {
       });
   }, []);
 
-var setWidth = 8
   return (
     <Layout>
       <h1 className="text-4xl uppercase text-center mt-16">
         Our <span className="font-bold">Clubs</span>
       </h1>
-      <div className="grid grid-cols-4 gap-3 py-10">
-        <div className="border-2 rounded-md w-36 h-40 bg-[#ABCEDF]">
-          <div><p><b>Card1</b></p></div> 
+        <div className={'grid grid-cols-4 gap-10 py-10'}>
+            <ClubListing />
+            <ClubListing />
+            <ClubListing />
+            <ClubListing />
+            <ClubListing />
+            <ClubListing />
+
         </div>
-        <div className="border-2 rounded-md w-36 h-40 bg-[#ABCEDF]">
-          Card2 
-        </div>
-        <div className="border-2 rounded-md w-36 h-40 bg-[#ABCEDF]">
-          Card3 
-        </div>
-        <div className="border-2 rounded-md w-36 h-40 bg-[#ABCEDF]">
-          Card4 
-        </div>
-        <div className="border-2 rounded-md w-36 h-40 bg-[#ABCEDF]">
-          Card5 
-        </div>
-        <div className="border-2 rounded-md w-36 h-40 bg-[#ABCEDF]">
-          Card6 
-        </div>
-      </div>
     </Layout>
   );
 };
