fd94cd30455fbf066c46801626aba8923aab26b9
Akash Shanmugaraj <65720968+akashShanmugraj@users.noreply.github.com>
2023-02-14 07:34:19 +0530

    Added Grid Layout

--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -19,28 +19,31 @@ const Clubs = () => {
       });
   }, []);
 
+var setWidth = 8
   return (
     <Layout>
       <h1 className="text-4xl uppercase text-center mt-16">
         Our <span className="font-bold">Clubs</span>
       </h1>
-      <div className="grid grid-cols-2 lg:grid-cols-6 gap-8 mt-8">
-        {clubs.map((club) => (
-          <div className="w-full">
-            <div className="w-full aspect-square border-gray-600 border p-8">
-              <div
-                className="w-full h-full hover:scale-110 transition-all duration-300"
-                style={{
-                  background: `url(${club.image_url})`,
-                  backgroundSize: "contain",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                }}
-              />
-            </div>
-            <p className="text-lg font-semibold font-sans mt-4">{club.clubName}</p>
-          </div>
-        ))}
+      <div className="grid grid-cols-4 gap-3 py-10">
+        <div className="border-2 rounded-md w-36 h-40 bg-[#ABCEDF]">
+          <div><p><b>Card1</b></p></div> 
+        </div>
+        <div className="border-2 rounded-md w-36 h-40 bg-[#ABCEDF]">
+          Card2 
+        </div>
+        <div className="border-2 rounded-md w-36 h-40 bg-[#ABCEDF]">
+          Card3 
+        </div>
+        <div className="border-2 rounded-md w-36 h-40 bg-[#ABCEDF]">
+          Card4 
+        </div>
+        <div className="border-2 rounded-md w-36 h-40 bg-[#ABCEDF]">
+          Card5 
+        </div>
+        <div className="border-2 rounded-md w-36 h-40 bg-[#ABCEDF]">
+          Card6 
+        </div>
       </div>
     </Layout>
   );
