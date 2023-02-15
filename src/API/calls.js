b78fda8ba12a5d80b392d21d27e9750aa43e326e
Akash Shanmugaraj <65720968+akashShanmugraj@users.noreply.github.com>
2023-02-15 21:44:45 +0530

    Added GET call function for Club Description

--- src/API/calls.js
+++ src/API/calls.js
@@ -3,3 +3,7 @@ import { API_URL } from "./config";
 
 export const fetchAllClubsLogo = () =>
   axios.get(`${API_URL}/clubs/with-logo`, {});
+
+export const fetchClubDescription= () =>
+  axios.get(`${API_URL}/clubs`, {});
+
