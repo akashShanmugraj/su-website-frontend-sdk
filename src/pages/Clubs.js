644c58784f5245e1ae073e9724b10586463591fc
Akash Shanmugaraj <65720968+akashShanmugraj@users.noreply.github.com>
2023-02-19 20:13:41 +0530

    Completed sucessful API Fetch

--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -4,7 +4,7 @@ import { TbExternalLink } from "react-icons/tb";
 import { Link } from "react-router-dom";
 import Layout from "./Layout.js";
 import { fetchAllClubsLogo } from "../API/calls.js";
-// import PersonList from "./ClubDescription.js";
+//? import PersonList from "./ClubDescription.js";
 
 function ClubListing(props) {
     return (
@@ -15,7 +15,7 @@ function ClubListing(props) {
                   </a>
                   <div className={"p-5"}>
                       <h5 className="mb-2 text-4xl font-bold tracking-tight text-gray-900">{props.clubName}</h5>
-                      <p class="mb-3 font-normal text-gray-700">{props.clubTagLine}</p>
+                      <p class="mb-3 font-normal text-gray-700">{props.category}</p>
                   </div>
               </div>
             </a>
@@ -24,20 +24,22 @@ function ClubListing(props) {
 
 
 const Clubs = () => {
-  const [clubs, setClubs] = useState([]);
+  const [clubs, setClubs] = useState({});
 
   useEffect(() => {
-    fetchAllClubsLogo()
+    axios.get('http://localhost:3002/api/clubs')
       .then((res) => {
-        console.log(res.data);
-        setClubs(res.data);
+        let clubData = res.data
+        setClubs(clubData)
+        // console.log(clubs)
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-
+var sampleClubData = {"_id":"63638b8e2f6d377ee358f1d7","clubName":"Civil Engineering Association","clubId":"cea","category":"Associations","__v":0}
+// console.log(sampleClubData.clubName)
 
   return (
     <Layout>
@@ -46,7 +48,7 @@ const Clubs = () => {
       </h1>
         <div className={'grid grid-cols-4 gap-10 py-10'}>
             <ClubListing clubName = "BRC CLub" clubImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Mercado_de_Col%C3%B3n%2C_Valencia%2C_Espa%C3%B1a%2C_2014-06-29%2C_DD_07.JPG/750px-Mercado_de_Col%C3%B3n%2C_Valencia%2C_Espa%C3%B1a%2C_2014-06-29%2C_DD_07.JPG"/>
-            <ClubListing />
+            {/* <ClubListing sampleClubData/> */}
             <ClubListing />
             <ClubListing />
             <ClubListing />
